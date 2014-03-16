4f4d7113e669495d6e26bc226c800178a3d64998
Daniel Boston <programmerdan@gmail.com>
2014-03-16 14:41:12 -0400

    master: All directions tested and good, starting golf process by removing string constants.

--- Twe.java
+++ Twe.java
@@ -25,15 +25,12 @@ class Twe {
 		int free,moves,x,y;
 		for(free=moves=y=0;y<4;y++){
 			for(x=0;x<4;x++){
-				if(board[y][x]<2){
-					chalk(""+y+","+x+":"+board[y][x],true);
-					free++;
-				}else if(x<3&&board[y][x]==board[y][x+1]||
+				if(board[y][x]<2) free++;
+				else if(x<3&&board[y][x]==board[y][x+1]||
 					    y<3&&board[y][x]==board[y+1][x])moves++;
 				if(board[y][x]>2047)return -1;
 			}
 		}
-		chalk(""+free+","+moves,true);
 		return free+moves;
 	}
 	void wrapup(int k){
@@ -56,7 +53,7 @@ class Twe {
 					if (fold(false,false)>0)return;
 					break;
 				case 'r':
-					if (foldRight()>0)return;
+					if (fold(true,false)>0)return;
 					break;
 				case 'z':
 					board[0][0]=2048; // instant win;
@@ -102,106 +99,6 @@ class Twe {
 		board=nb;
 		return didMove;
 	}
-
-
-	int foldUp(){
-		int didMove=0;
-		int nextSpot;
-		int[][] nb = new int[4][4];
-		for(int x=0;x<4;x++){
-			nextSpot=0;
-			for(int y=0;y<4;y++){
-				if(board[y][x]>0){
-					if(nb[nextSpot][x]<1){
-						nb[nextSpot][x]=board[y][x];
-						didMove+=y-nextSpot;
-					}else if(nb[nextSpot][x]==board[y][x]){
-						nb[nextSpot++][x]*=2;
-						didMove++;
-					}else{
-						nb[++nextSpot][x]=board[y][x];
-						didMove+=y-nextSpot;
-					}
-				}
-			}
-		}
-		board=nb;
-		return didMove;
-	}
-	int foldDown(){
-		int didMove=0;
-		int nextSpot;
-		int[][] nb = new int[4][4];
-		for(int x=0;x<4;x++){
-			nextSpot=3;
-			for(int y=3;y>=0;y--){
-				if(board[y][x]>0){
-					if(nb[nextSpot][x]<1){
-						nb[nextSpot][x]=board[y][x];
-						didMove+=nextSpot-y;
-					}else if(nb[nextSpot][x]==board[y][x]){
-						nb[nextSpot--][x]*=2;
-						didMove++;
-					}else{
-						nb[--nextSpot][x]=board[y][x];
-						didMove+=nextSpot-y;
-					}
-				}
-			}
-		}
-		board=nb;
-		return didMove;
-	}
-	int foldLeft(){
-		int didMove=0;
-		int nextSpot;
-		int[][] nb = new int[4][4];
-		for(int x=0;x<4;x++){
-			nextSpot=0;
-			for(int y=0;y<4;y++){
-				if(board[x][y]>0){
-					if(nb[x][nextSpot]<1){
-						nb[x][nextSpot]=board[x][y];
-						didMove+=y-nextSpot;
-					}else if(nb[x][nextSpot]==board[x][y]){
-						nb[x][nextSpot++]*=2;
-						didMove++;
-					}else{
-						nb[x][++nextSpot]=board[x][y];
-						didMove+=y-nextSpot;
-					}
-				}
-			}
-		}
-		chalk("fold left",true);
-		board=nb;
-		return didMove;
-	}
-	int foldRight(){
-		int didMove=0;
-		int nextSpot;
-		int[][] nb = new int[4][4];
-		for(int x=0;x<4;x++){
-			nextSpot=3;
-			for(int y=3;y>=0;y--){
-				if(board[x][y]>0){
-					if(nb[x][nextSpot]<1){
-						nb[x][nextSpot]=board[x][y];
-						didMove+=nextSpot-y;
-					}else if(nb[x][nextSpot]==board[x][y]){
-						nb[x][nextSpot--]*=2;
-						didMove++;
-					}else{
-						nb[x][--nextSpot]=board[x][y];
-						didMove+=nextSpot-y;
-					}
-				}
-			}
-		}
-		chalk("fold right",true);
-		board=nb;
-		return didMove;
-	}
 	int vec(){
 		return (new Random()).nextInt(4);
 	}
@@ -216,20 +113,26 @@ class Twe {
 	void chalk(String a, boolean nl){
 		System.out.print(a+(nl?"\n":""));
 	}
-	String buffer(){
-		return "|     |     |     |     |";
+	String fill(char node, char mid){
+		String str = ""+node;
+		for(int i=0;i<4;i++){
+			for(int j=0;j<5;j++)
+				str+=mid;
+			str+=node;
+		}
+		return str;
 	}
 	void drawBoard(){
-		chalk("+-----+-----+-----+-----+",true);
+		chalk(fill('+','-'),true);
 		String p[] = new String[6];
 		for(int[]y:board){
-			p[0]=p[1]=p[3]=p[4]=buffer();
+			p[0]=p[1]=p[3]=p[4]=fill('|',' ');
 			p[2]="";
 			for(int x=0;x<4;){
 				p[2]+=adjust(y[x++]);
 			}
 			p[2]+="|";
-			p[5]="+-----+-----+-----+-----+";
+			p[5]=fill('+','-');
 			for (String q:p){
 				chalk(q,true);
 			}
